054c6ff346f32428002220e865c4de66afe24927
Paul Walton <paul@denmandigital.com>
2021-05-17 13:56:27 -0700

    Replace DB script: replace JSON encoded urls in DB
    Replace JSON encoded URLs (such as in Gutenberg comments)

--- bin/restore_db.sh
+++ bin/restore_db.sh
@@ -11,5 +11,11 @@ cmd='exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE"'
 docker exec -i $(docker-compose ps -q db) sh -c "$cmd" < $file
 
 # Replace LIVE_URL using WP-CLI in wp container
-cmd='wp --allow-root --skip-themes --skip-plugins search-replace "$LIVE_URL" "$DEV_URL" --skip-columns=guid'
+# * Also replace JSON encoded URLs such as in Gutenberg comments
+cmd='
+JSON_LIVE_URL=$(php -r "echo json_encode(getenv(\"LIVE_URL\"));")
+JSON_DEV_URL=$(php -r "echo json_encode(getenv(\"DEV_URL\"));")
+wp --allow-root --skip-themes --skip-plugins search-replace "$LIVE_URL" "$DEV_URL" --skip-columns=guid
+wp --allow-root --skip-themes --skip-plugins search-replace "$JSON_LIVE_URL" "$JSON_DEV_URL" --skip-columns=guid
+'
 docker-compose exec wp sh -c "$cmd"
