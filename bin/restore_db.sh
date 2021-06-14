8d12a1cf5b85ab8a56b1fca03b652cbc8d80c5d0
Paul Walton <paul@denmandigital.com>
2021-06-14 15:30:38 -0700

    Replace DB script tweaks.
    - check for sql file existence
    - change var name from LIVE_URL to PROD_URL

--- bin/restore_db.sh
+++ bin/restore_db.sh
@@ -5,17 +5,22 @@ if [ -z "$file" ]; then
     echo "USAGE: restore-db <filename>"
     exit 1;
 fi
+if [ ! -f "$file" ]; then
+    echo "$file is not an existing file."
+    echo "USAGE: restore-db <filename>"
+    exit 1;
+fi
 
 # Restore database to db container
 cmd='exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE"'
 docker exec -i $(docker-compose ps -q db) sh -c "$cmd" < $file
 
-# Replace LIVE_URL using WP-CLI in wp container
+# Replace PROD_URL using WP-CLI in wp container
 # * Also replace JSON encoded URLs such as in Gutenberg comments
 cmd='
-JSON_LIVE_URL=$(php -r "echo json_encode(getenv(\"LIVE_URL\"));")
+JSON_PROD_URL=$(php -r "echo json_encode(getenv(\"PROD_URL\"));")
 JSON_DEV_URL=$(php -r "echo json_encode(getenv(\"DEV_URL\"));")
-wp --allow-root --skip-themes --skip-plugins search-replace "$LIVE_URL" "$DEV_URL" --skip-columns=guid
-wp --allow-root --skip-themes --skip-plugins search-replace "$JSON_LIVE_URL" "$JSON_DEV_URL" --skip-columns=guid
+wp --allow-root --skip-themes --skip-plugins search-replace "$PROD_URL" "$DEV_URL" --skip-columns=guid
+wp --allow-root --skip-themes --skip-plugins search-replace "$JSON_PROD_URL" "$JSON_DEV_URL" --skip-columns=guid
 '
 docker-compose exec wp sh -c "$cmd"
